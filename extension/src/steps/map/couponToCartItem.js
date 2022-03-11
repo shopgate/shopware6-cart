6ed62665845e9848b63453b549a163f1307c6f76
Konstantin Kiritsenko <k@componentk.com>
2022-03-11 20:47:22 +0300

    SGSW6-106 rule removal + tests

--- extension/src/steps/map/couponToCartItem.js
+++ extension/src/steps/map/couponToCartItem.js
@@ -10,7 +10,13 @@
 module.exports = async (context, input) => {
   const cartItemIds = input.swCart.lineItems
     .filter(lineItem => lineItem.type === 'promotion')
-    .filter(lineItem => input.couponCodes.includes(lineItem.referencedId))
+    .filter(lineItem => input.couponCodes.includes(
+      /**
+       * Auto-promo's do not have referencedId, therefore no coupon code is set.
+       * In this case the Theme sends lineItem ID of the promo instead of code
+       */
+      lineItem.referencedId === '' ? lineItem.id : lineItem.referencedId
+    ))
     .map(lineItem => lineItem.id)
 
   return { cartItemIds }
