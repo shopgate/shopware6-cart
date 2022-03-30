ea0b58cc57e0684337ac2fff9e612890ab8d5fe7
Rainer Skistims <rainer.skistims@shopgate.com>
2022-03-30 12:11:53 +0200

    SGSW6-107 update contextToken in favorites extension

---
+++ extension/src/steps/setContextTokenFavorites.js
@@ -0,0 +1,8 @@
+'use strict'
+
+/**
+ * This is a placeholder.
+ * The pipeline, which calls this function will be overwritten by the favorites extension, if it is deployed correctly.
+ * @returns {Promise<void>}
+ */
+module.exports = async () => {}
