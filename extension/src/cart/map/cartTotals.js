05f8065a4fc562b525c04d12cbbb81d2f2fe2df0
Konstantin Kiritsenko <k@componentk.com>
2025-02-21 22:00:22 +0700

    SG-336 cleanup

--- extension/src/cart/map/cartTotals.js
+++ extension/src/cart/map/cartTotals.js
@@ -1,20 +1,21 @@
 'use strict'
 
 const { Total, TotalsHandler } = require('../../services/totalsHandler')
+
 /**
  * @param {ApiteSW6Utility.PipelineContext} context
  * @param {ApiteSW6Cart.Input} input
  * @returns {Promise<{totals: []}>}
  */
 module.exports = async (context, input) => {
-  const { totalPrice, calculatedTaxes } = input.swCart.price
+  const { totalPrice, calculatedTaxes, positionPrice, netPrice } = input.swCart.price
   const totals = new TotalsHandler()
   // print for guest only when display shipping is enabled
   const displayShipping = context.meta.userId || context.config.displayGuestShipping
   const shipTax = input.swCart.deliveries.length ? input.swCart.deliveries[0].shippingCosts.calculatedTaxes.reduce((total, { tax }) => tax + total, 0.0) : 0.0
   if (totalPrice > 0) {
-    const grandTotal = displayShipping ? totalPrice : input.swCart.price.positionPrice
-    const subTotal = displayShipping ? input.swCart.price.netPrice : (input.swCart.price.netPrice - shipTax)
+    const grandTotal = displayShipping ? totalPrice : positionPrice
+    const subTotal = displayShipping ? netPrice : (netPrice - shipTax)
     const total = new Total('grandTotal', grandTotal)
     total.addSubtotal('subTotal', subTotal, 'NET')
     totals.addTotal(total)
